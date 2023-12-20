ae0f080f4a7b33f1dee03f4199c8d8a34b79995f
Juergen Neumann <juergen@junes.eu>
2023-12-20 19:12:11 +0100

    debug script error

--- deploy/dmx-test-ldap-login.sh
+++ deploy/dmx-test-ldap-login.sh
@@ -19,8 +19,8 @@ URL='core/topic/uri/dmx.workspaces.administration'
 BASE64="$( echo -n "${USERNAME}:${PASSWORD}" | base64 )"
 AUTH="Authorization: Basic ${BASE64}"
 SESSION="$( curl -sS -H "${AUTH}" "${HOST}/${URL}" -i 2>&1 )"
+echo "curl -sS -H "${AUTH}" "${HOST}/${URL}" -i 2>&1"
 HTTPCODE="$( echo "${SESSION}" | grep HTTP | cut -d' ' -f2 )"
-LDAPPASSWORD='testpass'
 if [ "${HTTPCODE}" != "200" -a "${HTTPCODE}" != "204" ]; then
     echo "login ${USERNAME} failed! (HTTPCODE=${HTTPCODE})"
     exit 1
